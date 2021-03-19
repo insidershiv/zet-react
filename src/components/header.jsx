29baa6bf4524f1fcd60a2f7cdf23f2e266507cb4
shiv <00shiv0000@gmail.com>
2021-03-19 18:15:02 +0530

    header added

---
+++ src/components/header.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+
+export default function Header() {
+
+    const useStyles = makeStyles((theme) => {
+
+    })
+    const classes = useStyles();
+    const theme = useTheme();
+    return (
+
+
+        <AppBar >
+            <Toolbar>
+
+            </Toolbar>
+
+        </AppBar>
+
+
+
+
+    )
+}
